ef33025497ff4035a9e78787f24307fe222d7c0b
vadim <V.L@s.afeka.ac.il>
2023-06-13 22:23:54 +0300

    Initial Commit

---
+++ Bloggy/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Bloggy
+//
+//  Created by Student29 on 13/06/2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
