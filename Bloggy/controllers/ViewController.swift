05a66c2cb93fd208ce2bf11cb87a5c854f8894f2
Vadim <V.L@s.afeka.ac.il>
2023-06-14 18:41:05 +0300

    another commit

--- Bloggy/controllers/ViewController.swift
+++ Bloggy/controllers/ViewController.swift
@@ -21,6 +21,8 @@ class ViewController: UIViewController ,UITableViewDataSource{
         blogs = Blog().mockData()
         tableVIewBlogs.dataSource = self
         
+        
+        
     }
     
     
